74decf52ab8bc30040639ecff0d7d05d4a939d6d
Paul Rowe <paul.louis.rowe@gmail.com>
2018-02-23 11:56:05 -0800

    adding routing

--- server.js
+++ server.js
@@ -1,49 +1,74 @@
-// Require/import the HTTP module
-// This, in essence, is a package ("small box") 
-//which allows our server ("big box") to have the 
-//capability of handling http requests and responses. 
-//HTTP is package that comes with the standard Node library.
+var fs = require("fs");
 var http = require("http");
 
-// Define a port to listen for incoming requests
-var PORT_GOOD = 7000;
-var PORT_BAD = 7500;
+//80: Hypertext Transfer Protocol (HTTP) used in the World Wide Web
+//registered ports are those from 1024 through 49151
+var PORT = 8080;
 
-// Create a generic function to handle requests and responses
-function handleGoodRequest(request, response) {
+var server = http.createServer(handleRequest);
 
-  // Send the below string to the client when the user visits the PORT URL
-  //res.send() will send the HTTP response
-  //res.end() will end the response process.
-  response.end("It Works!! Path Hit: " + request.url);
-}
-
-// Create a generic function to handle requests and responses
-function handleBadRequest(request, response) {
-
-  // Send the below string to the client when the user visits the PORT URL
-  //res.send() will send the HTTP response
-  //res.end() will end the response process.
-  response.end("It Works!! Path Hit: " + request.url);
-}
-
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var serverGood = http.createServer(handleGoodRequest);
-
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT_GOOD, function() {
-
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT_GOOD);
+// Start our server
+server.listen(PORT, function() {
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var serverBad = http.createServer(handleBadRequest);
+// Create a function which handles incoming requests and sends responses
+function handleRequest(req, res) {
 
-server.listen(PORT_BAD, function() {
+    // Capture the url the request is made to
+    var path = req.url;
 
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT_BAD);
-});
+
+    // Depending on the URL, display a different HTML file.
+    switch (path) {
+
+        case "/":
+            var file = "/index.html"
+            return serviceRequest(file, req, res);
+
+        case "/foods":
+            var file = "/foods.html"
+            return serviceRequest(file, req, res);
+
+        case "/movies":
+            var file = "/movies.html"
+            return serviceRequest(file, req, res);
+
+        case "/frameworks":
+            var file = "/frameworks.html"
+            return serviceRequest(file, req, res);
+
+        default:
+            return display404(path, req, res);
+    }
+}
+
+// When someone visits any path that is not specifically defined, this function is run.
+function display404(url, req, res) {
+    var myHTML = "<html>" +
+        "<body><h1>404 Not Found </h1>" +
+        "<p>The page you were looking for: " + url + " can not be found</p>" +
+        "</body></html>";
+
+    // Configure the response to return a status code of 404 (meaning the page/resource asked for couldn't be found), and to be an HTML document
+    res.writeHead(404, { "Content-Type": "text/html" });
+
+    // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+    res.end(myHTML);
+}
+
+// Create a function for handling the requests and responses coming into our server
+function serviceRequest(file, req, res) {
+
+    // Here we use the fs package to read our index.html file
+    fs.readFile(__dirname + file, function(err, data) {
+
+        // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+        // an html file.
+        res.writeHead(200, { "Content-Type": "text/html" });
+        //res.send() will send the HTTP response
+        //res.end() will end the response process.
+        res.end(data);
+    });
+}
